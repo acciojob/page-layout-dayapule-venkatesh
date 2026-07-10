89680758b842a2e03ee5d54a0188c9ab7f7a674c
dayapule-venkatesh <dayapulevenkatesh@gmail.com>
2026-07-10 10:39:05 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <h1>Welcome to my website</h1>
+      <p>This is the content of my website</p>
+      <p>© 2023 My Website. All rights reserved</p>
     </div>
   )
 }
